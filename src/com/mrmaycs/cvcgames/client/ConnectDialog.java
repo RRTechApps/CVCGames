fcaa2857d4042d99cf167a8b3c50202893ff0df4
Kenny Tang <kennyftang@gmail.com>
2017-02-23 16:12:30 -0600

    Working on connection dialog

--- src/com/mrmaycs/cvcgames/client/ConnectDialog.java
+++ src/com/mrmaycs/cvcgames/client/ConnectDialog.java
@@ -6,8 +6,11 @@ import javax.swing.*;
  * Round Rock Tech Apps 2017
  */
 public class ConnectDialog extends JDialog {
-
+    private JTextField netAddressField;
+    private JLabel netAddressLabel;
     public ConnectDialog() {
 
+        netAddressField = new JTextField();
+        netAddressLabel = new JLabel("Enter IP");
     }
 }
